ee4c81ec622d166f9f3109d4baf30d15657823c1
suvern <a821589498wmr>
2020-02-19 22:51:33 +0800

    Initial commit

---
+++ src/main/java/com/kddyzn/wenju/WenjuApplication.java
@@ -0,0 +1,13 @@
+package com.kddyzn.wenju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenjuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kddyzn/wenju/WenjuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kddyzn.wenju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WenjuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
